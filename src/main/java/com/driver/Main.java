39612a9a19b73a2244786685168ea5af8652c8f1
Tarun <tarun.rana1999@gmail.com>
2023-06-09 01:54:32 +0530

    Assignment Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw= new RWOnly();
+
+        rw.setName("Tarun");
+        // the name is private
+        String myName=rw.getName();
+        System.out.print(myName);
+    }
 }
